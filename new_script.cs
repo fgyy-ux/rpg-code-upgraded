29a23ac0804e7aa882cf225efabe5f67aea48597
fgyy-ux <arturcondratov@gmail.com>
2026-04-07 23:43:58 +0300

    initial commit for starting structures

---
+++ new_script.cs
@@ -0,0 +1,7 @@
+using System;
+using Godot;
+public class Player: Node2D;
+	public int maxHP = 100;
+	
+	public int playerLevel = 1;
+	public int playerXP = 0;
---
+++ rpg/player/player.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+public class Player: Note2D;
+public int playerMaxHp = 100;
+public int playerHp = 100;
+
+public int playerXp = 0;
+public int playerLevel = 1;
+public int 
